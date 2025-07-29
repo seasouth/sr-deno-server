26487a63e25efcd193e72f99f26f06d76e491c43
Cole <csouthern333@gmail.com>
2025-07-28 21:34:01 -0400

    Adding headers

--- main.ts
+++ main.ts
@@ -1,15 +1,28 @@
 import { handleWebSocket } from "./websocket.ts";
 
-console.log("Listening...");
-
 const port = parseInt(Deno.env.get("PORT") ?? "8000");
 
 Deno.serve({ port }, (req: Request) => {
   const { pathname } = new URL(req.url);
 
   if (pathname === "/ws") {
-    return handleWebSocket(req);
+    const headers = new Headers({
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+    });
+    try {
+      const response = handleWebSocket(req);
+      
+      headers.forEach((value, key) => {
+        response.headers.set(key, value);
+      });
+      return response;
+    } catch (error) {
+      console.error("Error handling WebSocket request:", error);
+      return new Response("Internal Server Error", { status: 500 });
+    }
   }
 
-  return new Response("Hello from Deno HTTP server!");
+  return new Response("Hello from Deno HTTP server!", { status: 200, headers: { "Content-Type": "text/plain" }});
 });
